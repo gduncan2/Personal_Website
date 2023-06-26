c5fd3d13997d6eb6c2c2d74f6dcb850fc773a355
Grant Duncan <gduncan2@illinois.edu>
2023-06-25 21:44:42 -0500

    Fresh wipe and new index.css done

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Nav from './components/Nav/Nav'
+import Experience from './components/Experience/Experience'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Nav.css'
+
+const Links = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Links
